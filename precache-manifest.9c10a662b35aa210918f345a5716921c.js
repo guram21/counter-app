2cbd88a686c6a5de1a1667e05e233b4e6e19c381
Guram Bautsadze <guram21@gmail.com>
2020-02-26 15:18:43 -0500

    Updates

---
+++ precache-manifest.9c10a662b35aa210918f345a5716921c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd3bbd05e4cccebbe374b579575f82f",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "f4b926239f4b479e5b09",
+    "url": "/counter-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d632718e8436ce2ceba6",
+    "url": "/counter-app/static/js/2.c0722f2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/counter-app/static/js/2.c0722f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b926239f4b479e5b09",
+    "url": "/counter-app/static/js/main.c08f5fef.chunk.js"
+  },
+  {
+    "revision": "329607e3f6d4259a7590",
+    "url": "/counter-app/static/js/runtime-main.d6d4e198.js"
+  }
+]);
